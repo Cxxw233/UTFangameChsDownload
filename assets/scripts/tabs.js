232fd2a76d3f1602115f08f3e066f46ea0830b44
Liu Wenyuan <15816141883@163.com>
2023-01-15 15:46:00 +0800

    table generator

--- assets/scripts/tabs.js
+++ assets/scripts/tabs.js
@@ -1,11 +1,15 @@
-let title = document.getElementsByClassName("title");
-let content = document.getElementsByClassName("label-table-content");
+let title = document.querySelectorAll("div.label > ul.title");
+let content = document.querySelectorAll("div.label > ul.label-table-content");
 for (let tabi = 0; tabi < title.length; tabi++) {
 	let titleelem = title[tabi];
 	let titlelist = titleelem.children;
 	let contentlist = content[tabi].children;
+	if (titlelist.length != contentlist.length || titlelist.length <= 0) {
+		console.error("发现了格式错误的表格！", titleelem);
+		continue;
+	}
 	function activateTab(i) {
-		for (let j = 0; j < titlelist.length; j++) {
+		for (let j = 0; j < contentlist.length; j++) {
 			contentlist[j].classList.add("label-table-content-hidden");
 		}
 		contentlist[i].classList.remove("label-table-content-hidden");
